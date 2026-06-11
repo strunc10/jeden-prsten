c8eb5fa687205160e99538bfca289a9e71301159
strunc10 <strunc10@gmail.com>
2026-06-11 19:35:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,29 @@ const passwordInput = document.getElementById("password");
 const enterBtn = document.getElementById("enterBtn");
 const response = document.getElementById("response");
 
-function tryEnter() {
+const correctHash =
+    "c1863b86e8fce94f051cf9c1e8b8f7f1cf7928f9a783b7f4f1c54c2f7f9d564c";
+
+async function sha256(text) {
+    const data = new TextEncoder().encode(text);
+
+    const hashBuffer = await crypto.subtle.digest(
+        "SHA-256",
+        data
+    );
+
+    return Array.from(new Uint8Array(hashBuffer))
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join("");
+}
+
+async function tryEnter() {
 
     const value = passwordInput.value.trim().toLowerCase();
 
-    if (value === "mellon") {
+    const enteredHash = await sha256(value);
+
+    if (enteredHash === correctHash) {
 
         response.className = "response opened";
 
